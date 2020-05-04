4fab15f81d1109b220395d4348ff9493969818b8
Chazensign <chazensign@gmail.com>
2020-05-04 07:41:29 -0600

    inital clean-up of create-react-app, added component, icons, and index.html modified

---
+++ src/PeopleDisplay.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react'
+
+class PeopleDisplay extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {  }
+  }
+  render() { 
+    return ( 
+      <section>
+
+      </section>
+     );
+  }
+}
+ 
+export default PeopleDisplay
